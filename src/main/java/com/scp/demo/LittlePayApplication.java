6452837060bcda9352d96400091875ddeb2f6fec
Kelly Zhu <kellyzhuscp@yahoo.com>
2025-05-10 11:22:23 +1000

    init commit

---
+++ src/main/java/com/scp/demo/LittlePayApplication.java
@@ -0,0 +1,13 @@
+package com.scp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittlePayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LittlePayApplication.class, args);
+	}
+
+}
